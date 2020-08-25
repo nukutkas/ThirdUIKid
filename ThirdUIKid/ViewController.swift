0e0eb203dd2fcd58199f7deaf746b588b81dcdf0
Nikita Kochetov <nothsmple@mail.ru>
2020-08-25 08:54:22 +0300

    Initial Commit

---
+++ ThirdUIKid/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ThirdUIKid
+//
+//  Created by Татьяна Кочетова on 25.08.2020.
+//  Copyright © 2020 Nikita Kochetov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
